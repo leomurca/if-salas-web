936d04114b3acd92f8871ca0a7b9b51cef9341dc
Leonardo Murça <leo@leomurca.xyz>
2022-08-28 16:26:13 -0300

    Revert mock files

--- src/services/mocks.js
+++ src/services/mocks.js
@@ -1,186 +1,186 @@
 const allClassrooms = [
-  // {
-  //   id: '321',
-  //   name: 'Introdução à Ciência de Dados',
-  //   abbreviation: 'ICD',
-  //   color: '#006FF2',
-  //   teachers: [
-  //     {
-  //       id: '2342',
-  //       name: 'Carlos Alexandre Silva',
-  //       avatar:
-  //         'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80',
-  //     },
-  //   ],
-  //   appointmentSlots: [
-  //     { weekDay: 'Quarta-feira', start: '10:00', end: '11:40' },
-  //     { weekDay: 'Sexta-feira', start: '10:00', end: '11:40' },
-  //   ],
-  // },
-  // {
-  //   id: '123',
-  //   name: 'Gestão de Projetos',
-  //   abbreviation: 'GP',
-  //   color: '#7900F2',
-  //   teachers: [
-  //     {
-  //       id: '1234',
-  //       name: 'Míriam Lúcia Barbosa',
-  //       avatar:
-  //         'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80',
-  //     },
-  //     {
-  //       id: '4321',
-  //       name: 'Alexandre Couto Cardoso',
-  //       avatar: '/assets/alex.jpg',
-  //     },
-  //   ],
-  //   appointmentSlots: [
-  //     { weekDay: 'Segunda-feira', start: '09:00', end: '10:40' },
-  //     { weekDay: 'Quinta-feira', start: '08:00', end: '09:00' },
-  //   ],
-  // },
-  // {
-  //   id: '666',
-  //   name: 'Banco de Dados II',
-  //   abbreviation: 'BDII',
-  //   color: '#FF7A00',
-  //   teachers: [
-  //     {
-  //       id: '6781',
-  //       name: 'Cristiane Norbiato Targa',
-  //       avatar:
-  //         'https://lh3.googleusercontent.com/a-/AOh14GhwNeQ0h2eKl2WXGuwyDzvLWtrvyrG2kJtZ7A1EBw=s75-c',
-  //     },
-  //   ],
-  //   appointmentSlots: [
-  //     { weekDay: 'Segunda-feira', start: '09:00', end: '10:40' },
-  //     { weekDay: 'Terça-feira', start: '08:00', end: '09:00' },
-  //   ],
-  // },
-  // {
-  //   id: '765',
-  //   name: 'Contabilidade Básica',
-  //   abbreviation: 'CB',
-  //   color: '#BB0000',
-  //   teachers: [
-  //     {
-  //       id: '4321',
-  //       name: 'Alexandre Couto Cardoso',
-  //       avatar: '/assets/alex.jpg',
-  //     },
-  //   ],
-  //   appointmentSlots: [
-  //     { weekDay: 'Sexta-feira', start: '20:00', end: '21:00' },
-  //     { weekDay: 'Terça-feira', start: '19:00', end: '20:10' },
-  //   ],
-  // },
-  // {
-  //   id: '333',
-  //   name: 'Linguagens de Programação',
-  //   abbreviation: 'LP',
-  //   color: '#039200',
-  //   teachers: [
-  //     {
-  //       id: '9999',
-  //       name: 'Gabriel Felipe Cândido Novy',
-  //       avatar:
-  //         'https://lh3.googleusercontent.com/a-/AOh14GgvfrD--cl25V_3UOAR93sN_jKdYNJ9PXhUH2zXhQ=s75-c',
-  //     },
-  //   ],
-  //   appointmentSlots: [
-  //     { weekDay: 'Terça-feira', start: '08:00', end: '09:20' },
-  //     { weekDay: 'Quarta-feira', start: '11:00', end: '12:20' },
-  //   ],
-  // },
+  {
+    id: '321',
+    name: 'Introdução à Ciência de Dados',
+    abbreviation: 'ICD',
+    color: '#006FF2',
+    teachers: [
+      {
+        id: '2342',
+        name: 'Carlos Alexandre Silva',
+        avatar:
+          'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80',
+      },
+    ],
+    appointmentSlots: [
+      { weekDay: 'Quarta-feira', start: '10:00', end: '11:40' },
+      { weekDay: 'Sexta-feira', start: '10:00', end: '11:40' },
+    ],
+  },
+  {
+    id: '123',
+    name: 'Gestão de Projetos',
+    abbreviation: 'GP',
+    color: '#7900F2',
+    teachers: [
+      {
+        id: '1234',
+        name: 'Míriam Lúcia Barbosa',
+        avatar:
+          'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50&q=80',
+      },
+      {
+        id: '4321',
+        name: 'Alexandre Couto Cardoso',
+        avatar: '/assets/alex.jpg',
+      },
+    ],
+    appointmentSlots: [
+      { weekDay: 'Segunda-feira', start: '09:00', end: '10:40' },
+      { weekDay: 'Quinta-feira', start: '08:00', end: '09:00' },
+    ],
+  },
+  {
+    id: '666',
+    name: 'Banco de Dados II',
+    abbreviation: 'BDII',
+    color: '#FF7A00',
+    teachers: [
+      {
+        id: '6781',
+        name: 'Cristiane Norbiato Targa',
+        avatar:
+          'https://lh3.googleusercontent.com/a-/AOh14GhwNeQ0h2eKl2WXGuwyDzvLWtrvyrG2kJtZ7A1EBw=s75-c',
+      },
+    ],
+    appointmentSlots: [
+      { weekDay: 'Segunda-feira', start: '09:00', end: '10:40' },
+      { weekDay: 'Terça-feira', start: '08:00', end: '09:00' },
+    ],
+  },
+  {
+    id: '765',
+    name: 'Contabilidade Básica',
+    abbreviation: 'CB',
+    color: '#BB0000',
+    teachers: [
+      {
+        id: '4321',
+        name: 'Alexandre Couto Cardoso',
+        avatar: '/assets/alex.jpg',
+      },
+    ],
+    appointmentSlots: [
+      { weekDay: 'Sexta-feira', start: '20:00', end: '21:00' },
+      { weekDay: 'Terça-feira', start: '19:00', end: '20:10' },
+    ],
+  },
+  {
+    id: '333',
+    name: 'Linguagens de Programação',
+    abbreviation: 'LP',
+    color: '#039200',
+    teachers: [
+      {
+        id: '9999',
+        name: 'Gabriel Felipe Cândido Novy',
+        avatar:
+          'https://lh3.googleusercontent.com/a-/AOh14GgvfrD--cl25V_3UOAR93sN_jKdYNJ9PXhUH2zXhQ=s75-c',
+      },
+    ],
+    appointmentSlots: [
+      { weekDay: 'Terça-feira', start: '08:00', end: '09:20' },
+      { weekDay: 'Quarta-feira', start: '11:00', end: '12:20' },
+    ],
+  },
 ];
 
 const allAssignments = [
-  // {
-  //   id: '5435',
-  //   type: 'assessment',
-  //   title:
-  //     'Prova 1 - Armazenamento de Dados. Python em CD. Armazenamento Analítico',
-  //   dueDate: '2022-07-01 23:59',
-  //   scores: [
-  //     {
-  //       classroomId: '321',
-  //       value: 30,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '321'),
-  // },
-  // {
-  //   id: '0128',
-  //   type: 'assessment',
-  //   title:
-  //     'Prova 2 - Visualização de Dados. Matemática e Estatística em CD. Análise de Dados',
-  //   dueDate: '2022-09-01 23:59',
-  //   scores: [
-  //     {
-  //       classroomId: '321',
-  //       value: 30,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '321'),
-  // },
-  // {
-  //   id: '1234',
-  //   type: 'project',
-  //   title: 'Trabalho NoSQL',
-  //   dueDate: '2022-06-29 22:00',
-  //   scores: [
-  //     {
-  //       classroomId: '666',
-  //       value: 35,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '666'),
-  // },
-  // {
-  //   id: '1234',
-  //   type: 'assessment',
-  //   title: 'Atividade 2 - Estudo de caso Sapiens Informática',
-  //   dueDate: '2022-06-25 23:59',
-  //   scores: [
-  //     {
-  //       classroomId: '123',
-  //       value: 10,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '123'),
-  // },
-  // {
-  //   id: '3671',
-  //   type: 'assessment',
-  //   title:
-  //     'AA08 - Atividade de Aprendizagem 08 - Componentes de rateio de custos',
-  //   dueDate: '2022-07-23 10:00',
-  //   scores: [
-  //     {
-  //       classroomId: '765',
-  //       value: 1,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '765'),
-  // },
-  // {
-  //   id: '1717',
-  //   type: 'project',
-  //   title: 'Trabalho interdisciplinar',
-  //   dueDate: '2022-08-20 23:59',
-  //   scores: [
-  //     {
-  //       classroomId: '666',
-  //       value: 20,
-  //     },
-  //     {
-  //       classroomId: '321',
-  //       value: 30,
-  //     },
-  //   ],
-  //   classrooms: allClassrooms.filter(c => c.id === '666' || c.id === '321'),
-  // },
+  {
+    id: '5435',
+    type: 'assessment',
+    title:
+      'Prova 1 - Armazenamento de Dados. Python em CD. Armazenamento Analítico',
+    dueDate: '2022-07-01 23:59',
+    scores: [
+      {
+        classroomId: '321',
+        value: 30,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '321'),
+  },
+  {
+    id: '0128',
+    type: 'assessment',
+    title:
+      'Prova 2 - Visualização de Dados. Matemática e Estatística em CD. Análise de Dados',
+    dueDate: '2022-09-01 23:59',
+    scores: [
+      {
+        classroomId: '321',
+        value: 30,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '321'),
+  },
+  {
+    id: '1234',
+    type: 'project',
+    title: 'Trabalho NoSQL',
+    dueDate: '2022-06-29 22:00',
+    scores: [
+      {
+        classroomId: '666',
+        value: 35,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '666'),
+  },
+  {
+    id: '1234',
+    type: 'assessment',
+    title: 'Atividade 2 - Estudo de caso Sapiens Informática',
+    dueDate: '2022-06-25 23:59',
+    scores: [
+      {
+        classroomId: '123',
+        value: 10,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '123'),
+  },
+  {
+    id: '3671',
+    type: 'assessment',
+    title:
+      'AA08 - Atividade de Aprendizagem 08 - Componentes de rateio de custos',
+    dueDate: '2022-07-23 10:00',
+    scores: [
+      {
+        classroomId: '765',
+        value: 1,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '765'),
+  },
+  {
+    id: '1717',
+    type: 'project',
+    title: 'Trabalho interdisciplinar',
+    dueDate: '2022-08-20 23:59',
+    scores: [
+      {
+        classroomId: '666',
+        value: 20,
+      },
+      {
+        classroomId: '321',
+        value: 30,
+      },
+    ],
+    classrooms: allClassrooms.filter(c => c.id === '666' || c.id === '321'),
+  },
 ];
 
 const allClassroomAnnouncements = [
