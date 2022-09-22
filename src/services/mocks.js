afadb2415e53a6ed9f88bb02fc3ecd1122dff74d
Leonardo Murça <leo@leomurca.xyz>
2022-09-22 10:18:26 -0300

    Testing workflow

--- src/services/mocks.js
+++ src/services/mocks.js
@@ -540,7 +540,7 @@ const user = {
   username: 'josedasilva',
   email: 'jose@gmail.com',
   password: '#jose1234', // TODO: Remove this!
-  firstName: 'Arthur',
+  firstName: 'Fernando',
   lastName: 'Silva',
   token: 'skdfb9458hnsdfsif4w38r9234ry98423',
   phone: '31999999999',
