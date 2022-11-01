07756f89947cc9f7f95257eb2f5b7c057233932f
Leonardo Murça <leo@leomurca.xyz>
2022-11-01 11:37:01 -0300

    Fix line break on deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,5 @@
 mkdir ~/.ssh &&
 echo "$SSH_KEY" >> ~/.ssh/id_rsa_ifsalas &&
 chmod 400 ~/.ssh/id_rsa_ifsalas &&
-echo -e "
-  Host ifsalas\n\t \
-    User ifsalas\n\t \
-    Hostname 45.76.5.44\n\t \
-    IdentityFile ~/.ssh/id_rsa_ifsalas\n\t \
-    StrictHostKeyChecking No" >> ~/.ssh/config &&
+echo -e "Host ifsalas\n\tUser ifsalas\n\tHostname 45.76.5.44\n\tIdentityFile ~/.ssh/id_rsa_ifsalas\n\tStrictHostKeyChecking No" >> ~/.ssh/config &&
 rsync -avz --progress build/ ifsalas:/home/ifsalas/web/$1 --delete
