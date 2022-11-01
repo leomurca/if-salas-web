1285693ac822ebc44ef31282d9cc04eec60eba1f
Leonardo Murça <leo@leomurca.xyz>
2022-11-01 11:41:53 -0300

    Pass SSH_KEY as parameter

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir ~/.ssh &&
-echo "$SSH_KEY" >> ~/.ssh/id_rsa_ifsalas &&
+echo "$1" >> ~/.ssh/id_rsa_ifsalas &&
 chmod 400 ~/.ssh/id_rsa_ifsalas &&
 echo -e "Host ifsalas\n\tUser ifsalas\n\tHostname 45.76.5.44\n\tIdentityFile ~/.ssh/id_rsa_ifsalas\n\tStrictHostKeyChecking No" >> ~/.ssh/config &&
-rsync -avz --progress build/ ifsalas:/home/ifsalas/web/$1 --delete
+rsync -avz --progress build/ ifsalas:/home/ifsalas/web/$2 --delete
