c695923918489276621e3b202371c44aa64c7d77
Leonardo Murça <leo@leomurca.xyz>
2024-08-01 11:45:24 -0300

    Add home unlogged page

--- src/screens/UnauthenticatedHome/index.js
+++ src/screens/UnauthenticatedHome/index.js
@@ -1,17 +1,58 @@
-import { Link } from '@mui/material';
+import { Button, Link } from '@mui/material';
 import { Container } from '@mui/system';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
+import screenshot from '../../assets/screenshot.png';
 
 import styles from './styles';
+import useLayoutType from '../../hooks/useLayoutType';
 
 function UnauthenticatedHome() {
-  useDocumentTitle('Seja bem-vindo');
-  const { container } = styles.desktop;
+  useDocumentTitle('IF Salas');
+  const layoutType = useLayoutType();
+  const {
+    container,
+    containerLeft,
+    containerRight,
+    containerLeftH1,
+    containerLeftP,
+    buttonsContainer,
+    button,
+    img,
+  } = styles[layoutType];
 
   return (
     <Container sx={container} maxWidth="false">
-      <h1>Seja bem-vindo!</h1>
-      <Link href="/login">Ir para login</Link>
+      <Container sx={containerLeft}>
+        <h1 style={containerLeftH1}>A plataforma de ensino de código aberto</h1>
+        <p style={containerLeftP}>
+          Descubra o IF Salas, a plataforma de ensino inovadora do Instituto
+          Federal de Minas Gerais (IFMG). Com o IF Salas, alunos e professores
+          têm acesso a um ambiente virtual de aprendizado dinâmico e interativo,
+          onde é possível participar de aulas ao vivo, acessar materiais
+          didáticos, realizar atividades e avaliações, e colaborar em projetos
+          de forma simples e eficiente.
+        </p>
+
+        <Container style={buttonsContainer}>
+          <Button style={button} variant="contained" href="login">
+            Fazer login no IF Salas
+          </Button>
+
+          <Button style={button} variant="outlined" href="register">
+            Registrar-se no IF Salas
+          </Button>
+        </Container>
+      </Container>
+
+      <Container sx={containerRight}>
+        <img
+          style={img}
+          src={screenshot}
+          alt="Print da plartaforma"
+          width="650"
+          height="550"
+        />
+      </Container>
     </Container>
   );
 }
--- src/screens/UnauthenticatedHome/styles.js
+++ src/screens/UnauthenticatedHome/styles.js
@@ -1,16 +1,137 @@
+// ========== Desktop  ==========
 const desktopContainer = {
-  height: '100vh',
+  width: '85%',
+  display: 'flex',
   margin: 0,
+  paddingTop: '100px',
+};
+
+const desktopContainerLeft = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+};
+
+const desktopContainerLeftH1 = {
+  fontSize: '60px',
+};
+
+const desktopContainerLeftP = {
+  fontSize: '20px',
+  marginTop: '0',
+};
+
+const desktopContainerRight = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alginItems: 'center',
+  justifyContent: 'center',
+};
+
+const desktopButtonsContainer = {
+  marginTop: '20px',
   padding: 0,
   display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  alignItems: 'center',
+  gap: '30px',
+};
+
+const desktopButton = {
+  width: 'fit-content',
+  padding: '10px 20px',
+};
+
+const desktopImg = {
+  margin: '0 auto',
 };
 
 const desktop = {
   container: desktopContainer,
+  containerLeft: desktopContainerLeft,
+  containerRight: desktopContainerRight,
+  containerLeftH1: desktopContainerLeftH1,
+  containerLeftP: desktopContainerLeftP,
+  buttonsContainer: desktopButtonsContainer,
+  button: desktopButton,
+  img: desktopImg,
 };
 
-const styles = { desktop };
+// ========== Mobile ==========
+const mobileContainer = {
+  width: '100%',
+  display: 'flex',
+  margin: 0,
+  paddingTop: '20px',
+  flexDirection: 'column',
+};
+
+const mobileContainerLeft = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+};
+
+const mobileContainerRight = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  alginItems: 'center',
+  justifyContent: 'center',
+};
+
+const mobileContainerLeftH1 = {
+  fontSize: '40px',
+};
+
+const mobileContainerLeftP = {
+  fontSize: '15px',
+  margin: 0,
+};
+
+const mobileButton = {
+  width: '100%',
+};
+
+const mobileButtonsContainer = {
+  marginTop: '20px',
+  padding: 0,
+  display: 'flex',
+  gap: '30px',
+  flexDirection: 'column',
+  width: '100%',
+};
+
+const mobileImg = {
+  width: '100%',
+  height: '250px',
+  margin: '0 auto',
+  marginTop: '50px',
+};
+
+const mobile = {
+  container: mobileContainer,
+  containerLeft: mobileContainerLeft,
+  containerRight: mobileContainerRight,
+  containerLeftH1: mobileContainerLeftH1,
+  containerLeftP: mobileContainerLeftP,
+  buttonsContainer: mobileButtonsContainer,
+  button: mobileButton,
+  img: mobileImg,
+};
+
+// ========== Unset ==========
+const unset = {
+  container: null,
+  containerLeft: null,
+  containerRight: null,
+  containerLeftH1: null,
+  containerLeftP: null,
+  buttonsContainer: null,
+  button: null,
+  img: null,
+};
+
+const styles = { desktop, mobile, unset };
 export default styles;
